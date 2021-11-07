bf9790923228a2924161b46d6d95796b5dfc7123
Andrew Tran <tranandrew@gmail.com>
2021-11-06 22:54:49 -0500

    completed api/index.js. added template to other routes

---
+++ Develop/routes/api/index.js
@@ -0,0 +1,11 @@
+//Boiler plate
+const router = require("express").Router();
+const categoryRoutes = require("./categoryRoutes");
+const productRoutes = require("./productRoutes");
+const tagRoutes = require("./tagRoutes");
+
+router.use("/category", categoryRoutes);
+router.use("/products", productRoutes);
+router.use("/tags", tagRoutes);
+
+module.exports = router;
