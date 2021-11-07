931a82047a36b85712aedeb99c7dfc60ddd19b72
Andrew Tran <tranandrew@gmail.com>
2021-11-06 23:10:13 -0500

    Added models/index.js. Will edit for cascade and check (through ProductTag)

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,16 +1,31 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-
+Product.belongsto(Category, {
+  foreignKey: "",
+  onDelete: "CASCADE",
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "",
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(ProductTag, {
+  foreignKey: "",
+  onDelete: "CASCADE",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(ProductTag, {
+  foreignKey: "",
+  onDelete: "CASCADE",
+});
 
 module.exports = {
   Product,
