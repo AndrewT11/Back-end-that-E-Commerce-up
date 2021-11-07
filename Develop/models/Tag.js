f0427451d4578110c9201776a818a6e53289f1cd
Andrew Tran <tranandrew@gmail.com>
2021-11-07 17:15:33 -0600

    template for models added

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -1,19 +1,44 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Tag extends Model {}
 
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    make: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    model: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    mileage: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    driver_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "driver",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: "tag",
   }
 );
 
